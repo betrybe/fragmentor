69bef3df30a3cf860cdc85558b8e3361c8bb7c0a
Victor Caciquinho Pereira <victorcaciquinho@gmail.com>
2022-04-08 13:49:20 -0300

    feat: Project initial setup

---
+++ lib/fragmentor.ex
@@ -0,0 +1,5 @@
+defmodule Fragmentor do
+  @moduledoc """
+  ## Fragmentor
+  """
+end
---
+++ mix.exs
@@ -0,0 +1,52 @@
+defmodule Fragmentor.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :fragmentor,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package(),
+      description: description()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp description do
+    "Fragmentor is an package for parsing and breaking markdown content into html fragments."
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:earmark, "~> 1.4.24"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: [
+        "Víctor Caciquinho Pereira",
+      ],
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/betrybe/fragmentor/"}
+    ]
+  end
+end
