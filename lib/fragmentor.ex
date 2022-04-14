60a78a23305864f1b06aefcf84e04c72d67192bb
Victor Caciquinho Pereira <victorcaciquinho@gmail.com>
2022-04-14 09:08:22 -0300

    feat: creates fragmentor interface with delegations

--- lib/fragmentor.ex
+++ lib/fragmentor.ex
@@ -1,5 +1,25 @@
 defmodule Fragmentor do
   @moduledoc """
-  ## Fragmentor
+  Fragmentor is library made for converting markdown into
+  html fragmented by separated types.
+
+  Currently we support only three types of fragments
+  * `Code` - Consists of Syntax Highlighting Codes
+  * `HTML` - Any kind of HTML code with tags and text
+  * `Video` - Consists of video html tags and its atributes
   """
+
+  @doc """
+  Converts Markdown text directly to a fragmented HTML
+  """
+  defdelegate to_fragments(markdown, options \\ []), to: Fragmentor.Processor
+
+  @doc """
+  Convert Markdown text directly to raw HTML
+
+  ## Example
+      iex> to_html("*markdown content*", compact_output: true)
+      "<p><em>markdown content</em></p>"
+  """
+  defdelegate to_html(markdown, options \\ []), to: Fragmentor.Processor
 end
