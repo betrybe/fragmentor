e320e2a2ffc99c57b3f7b170b74067267edc2f4b
Victor Caciquinho Pereira <victorcaciquinho@gmail.com>
2022-04-14 12:48:27 -0300

    feat: adiciona parsing com bang operator

--- lib/fragmentor/processor.ex
+++ lib/fragmentor/processor.ex
@@ -7,10 +7,10 @@ defmodule Fragmentor.Processor do
   alias Fragmentor.Parser.HtmlParser
   alias Fragmentor.Fragment.{Code, Video, Html}
 
-  @spec to_html(String.t(), list()) ::
+  @spec to_html(binary(), list()) ::
           {:error, list} | {:error, String.t()} | {:ok, binary}
 
-  def to_html(markdown, options \\ [])
+  def to_html(markdown, options \\ [compact_output: true])
 
   def to_html(nil, _options), do: {:error, "Markdown Empty"}
 
@@ -23,15 +23,33 @@ defmodule Fragmentor.Processor do
     end
   end
 
-  @spec to_fragments(binary(), list()) :: [Code.t() | Video.t() | Html.t()]
-  def to_fragments(markdown, options \\ []) do
-    {:ok, html} = to_html(markdown, options)
+  @spec to_html!(binary(), list()) :: binary()
+  def to_html!(markdown, options \\ [compact_output: true])
 
-    build_fragments(html)
+  def to_html!(nil, _options), do: {:error, "Markdown Empty"}
+
+  def to_html!(markdown, options) do
+    markdown
+    |> Earmark.as_html!(options)
+    |> Utils.remove_hr_inner_class()
   end
 
-  defp build_fragments(markdown) when is_binary(markdown) do
-    {:ok, html_content} = to_html(markdown)
-    HtmlParser.to_fragments(html_content)
-  end
+
+  ## ToDo Adicionar tratamento de erro aqui
+  @spec to_fragments(binary(), list()) ::
+          {:ok, [Code.t() | Video.t() | Html.t()]} | {:error, String.t()}
+  def to_fragments(html, _options \\ []), do: {:ok, HtmlParser.to_fragments(html)}
+
+  @spec to_fragments!(binary(), list()) :: [Code.t() | Video.t() | Html.t()]
+  def to_fragments!(html, _options \\ []), do: HtmlParser.to_fragments(html)
+
+  ## ToDo Add a markdown -> html -> fragment flux
+  # def to_fragments(markdown, options \\ []) do
+  #   with {:ok, html_content} <- to_html(markdown, options),
+  #        fragmented_html <- HtmlParser.to_fragments(html_content) do
+  #     {:ok, fragmented_html}
+  #   else
+  #     {:error, error_message} -> {:error, error_message}
+  #   end
+  # end
 end
