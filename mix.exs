6d20ea7adb15a93b1d9babe1f93bec515c672b44
Ígor Chagas <igor.chagas@betrybe.com>
2022-07-18 16:50:12 -0300

    fix: update version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fragmentor.MixProject do
   def project do
     [
       app: :fragmentor,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
